0bb16aaf97ded522624825de917603f9593ff8e4
Emmanuel Durojaiye <elijah2190@icloud.com>
2025-09-18 01:03:53 -0400

    Add files via upload

---
+++ Lab 1/Lab1A.java
@@ -0,0 +1,24 @@
+/*
+Class: CSE 1321L
+Section: 1
+Term: Spring 2024
+Instructor: Eun Sik Kim
+Name:Emmanuel Durojaiye
+Lab#: 1
+*/
+
+// Program Lab1A
+// Demonstrate the difference between print and println
+public class Lab1A
+{
+    // Prints two lines of output representing a rocket countdown.
+    public static void main (String[] args)
+    {
+        System.out.print ("Three... ");
+        System.out.print ("Two... ");
+        System.out.print ("One... ");
+        System.out.print ("Zero... ");
+        System.out.println ("Liftoff!"); // appears on first line
+        System.out.println ("Houston, we have a problem.");
+    }
+}
---
+++ Lab 1/Lab1B.java
@@ -0,0 +1,24 @@
+/*
+Class: CSE 1321L
+Section: 1
+Term: Spring 2024
+Instructor: Eun Sik Kim
+Name:Emmanuel Durojaiye
+Lab#: 1
+*/
+
+// Program Lab1B.java
+// Demonstrate reading a string from the user.
+import java.util.Scanner;
+public class Lab1B
+{
+    // Reads a character string from the user and prints it.
+    public static void main (String[] args)
+    {
+        String message;
+        Scanner scan = new Scanner (System.in);
+        System.out.print ("Enter a line of text: ");
+        message = scan.nextLine();
+        System.out.println ("You wrote '" + message + "'");
+    }
+}
